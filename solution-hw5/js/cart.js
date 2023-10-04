b894cfae49ceda4449129adaab2077464ddbc101
vivianli0 <ruijunl@andrew.cmu.edu>
2023-10-04 11:11:43 -0400

    success *tears*

--- solution-hw5/js/cart.js
+++ solution-hw5/js/cart.js
@@ -1,5 +1,7 @@
 // initiative shopping cart
 let cart = [];
+// for calculating final price
+let priceList = [];
 
 // define Roll class
 class Roll {
@@ -8,6 +10,28 @@ class Roll {
         this.glazing =  rollGlazing;
         this.size = packSize;
         this.basePrice = rollPrice;
+    }
+}
+
+// for calculating final price
+// I know it's better to reference the objects in main.js
+// but I didn't want to go back and change the entire code
+// because my adaptations are in arrays --> something on my to-do list 
+const precision = 2;
+const priceAdjust = {
+
+    size: {
+        1: 1,
+        3: 3, 
+        6: 5, 
+        12: 10
+    },
+
+    glazing: {
+        "Original": 0,
+        "Sugar Milk": 0,
+        "Vanilla Milk": 0.5,
+        "Double Chocolate": 1.5
     }
 }
 
@@ -22,6 +46,7 @@ addNewRoll("Original", "Sugar Milk", 1, rolls.Original.basePrice);
 addNewRoll("Walnut", "Vanilla Milk", 12, rolls.Walnut.basePrice);
 addNewRoll("Raisin", "Sugar Milk", 3, rolls.Raisin.basePrice);
 addNewRoll("Apple", "Original", 3, rolls.Apple.basePrice);
+
 
 // display cart items, update & delete   
 // REMEMBER! this function is for each item, then iterate w/ for loop through the cart array
@@ -42,6 +67,7 @@ function createElement(item) {
 
     // delete button 
     const deleteBtn = item.element.querySelector(".remove-btn");
+    deleteBtn.style.cursor = "pointer";
     deleteBtn.addEventListener("click", () => {
         deleteItem(item);
     })
@@ -60,12 +86,43 @@ function updateElement(item) {
     type.innerText = item.type + " Cinnamon Roll";
     glazing.innerText = "Glazing: " + item.glazing;
     size.innerText = "Pack Size: " + item.size;
-    price.innerHTML = "<br> $" + item.basePrice;
+    const totalPrice = (item.basePrice + priceAdjust.glazing[item.glazing]) * priceAdjust.size[item.size];
+    price.innerHTML = "<br> $" + totalPrice.toFixed(precision);
+
+    // add to price list array
+    priceList.push(totalPrice.toFixed(precision));
+
+    // proof reading 
+    //console.log(item.basePrice + ' + ' + priceAdjust.glazing[item.glazing] + ' x ' + priceAdjust.size[item.size] + ' = ' + totalPrice.toFixed(precision));
 }
 
+function deleteItem(item) {
+    item.element.remove();
+    const index = cart.indexOf(item);
+    cart.splice(index, 1);
+    priceList.splice(index, 1);
+
+    updateFinalPrice();
+}
+
+function updateFinalPrice() {
+    let total = 0;
+    for (let i = 0; i < priceList.length; i++) {
+    total += parseFloat(priceList[i]);
+    }
+
+    let element = document.querySelector('.num'); 
+    element.innerText = "$" + total.toFixed(precision);
+}
+
+// let's goooo!
 for (let i = 0; i < cart.length; i++) {
     createElement(cart[i]);
+    updateFinalPrice();
 }
+
+
+
 
 
 
